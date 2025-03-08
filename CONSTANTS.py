f1799fc616c07225bbc9a0f88fc0ad31d02ab167
Teckt <36281089+Teckt@users.noreply.github.com>
2025-03-08 15:26:11 -0500

    requirements

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,25 +1,23 @@
 import os
 
 TEXT_ENCODER_CLIP_L_PATH='openai/clip-vit-large-patch14'
-FLUX_FILL_HYPER_PATH='flux-fill-fp8'
-FLUX_HYPER_PATH='flux-fp8'
 FLUX_FILL_PATH='black-forest-labs/FLUX.1-Fill-dev'
 FLUX_PATH='black-forest-labs/FLUX.1-dev'
-COG_PATH='THUDM/CogVideoX-5b-I2V'
-USE_OPTIMUM_QUANTO=True
-USE_CPU_OFFLOAD=False
-USE_SEQUENTIAL_CPU_OFFLOAD=False
-FUSE_HYPER=False
-FUSE_HYPER_ALPHA=0.125
-FUSE_HYPER_REPO='ByteDance/Hyper-SD'
-FUSE_HYPER_MODEL_FILE='Hyper-FLUX.1-dev-8steps-lora.safetensors'
-DOWNLOAD_FILES=True
-# the dir to look for lora files
-LORA_PATH="C:\\Users\\teckt\\PycharmProjects\\kohya\\kohya_ss\\training_data\\model"
-# the default name to give for every output image file
-REPAINT_OUTPUT_DIR="redressed_results"
-IMAGE_OUTPUT_DIR="generated_results"
-OUTPUT_FILE_BASE_NAME="outputImage.png"
-CRED_PATH='firebase_cred.json'
-STORAGE_BUCKET_ID='df-studio-1.appspot.com'
-JOB_DIR = os.path.join('C:' + os.sep, 'deepfakes', 'df-maker-files')
+COG_PATH='THUDM/CogVideoX-5b-I2V'  # cogxvideo repo
+USE_OPTIMUM_QUANTO=True  # quantize with optimum quanto
+USE_CPU_OFFLOAD=False  # vram savings
+USE_SEQUENTIAL_CPU_OFFLOAD=False  # bigger vram savings
+FUSE_HYPER=False  # whether to fuse hyper lora with transformer
+FLUX_FILL_HYPER_PATH='flux-fill-fp8'  # the pretrained path to the fused model
+FLUX_HYPER_PATH='flux-fp8'  # the pretrained path to the fused model
+FUSE_HYPER_ALPHA=0.125  # set alpha for turbo lora
+FUSE_HYPER_REPO='ByteDance/Hyper-SD'  #repo to hyper model
+FUSE_HYPER_MODEL_FILE='Hyper-FLUX.1-dev-8steps-lora.safetensors'  #file name to hyper model
+DOWNLOAD_FILES=True  # whether to download models
+LORA_PATH="C:\\Users\\teckt\\PycharmProjects\\kohya\\kohya_ss\\training_data\\model" # the dir to look for lora files
+REPAINT_OUTPUT_DIR="redressed_results"  # dir to store repainted images
+IMAGE_OUTPUT_DIR="generated_results"  # dit to store generated images
+OUTPUT_FILE_BASE_NAME="outputImage.png" # the default name to give for every output image file
+CRED_PATH='firebase_cred.json'  # firebase credentials
+STORAGE_BUCKET_ID='df-studio-1.appspot.com'  # fire storage bucket id
+JOB_DIR = os.path.join('C:' + os.sep, 'deepfakes', 'df-maker-files')  # the directory to store DFS files
---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# install python3.11
+
+# sudo yum install git
+
+# sudo yum install mesa-libGL  # for cv2
+
+# install pytorch 2.6
+
+# mkdir /app
+
+# cd app
+
+# python3.11 venv .venv  # create virtual environment
+
+# source .venv/bin/activate  # activate virtual environment
+
+# clone dfs-seg-inpaint-swap
+
+# cd
+
+# pip install -r requirements.txt
+
+# run test.py save firebase_cred.json
+
+
+
+FROM baseImage
+CMD []
