35b6da0169a723b45026ecb36b2a9600dea87fd9
Teckt <36281089+Teckt@users.noreply.github.com>
2025-03-29 17:05:38 -0500

    Update redresser_flux.py

--- redresser_flux.py
+++ redresser_flux.py
@@ -74,11 +74,16 @@ class Redresser:
             for (face_index, face_data) in yolo_result.items():
                 if face_index not in yolo_results[image_index].keys():
                     yolo_results[image_index][face_index] = {}
-                aligned_cropped_image = load_image(face_data['aligned_cropped_image_path'], "numpy")
-                seg_mask = load_image(face_data['seg_mask_path'], "numpy")
+                if 'aligned_cropped_image' in face_data.keys():
+                    aligned_cropped_image = load_image(face_data['aligned_cropped_image_path'], "numpy")
+                    seg_mask = load_image(face_data['seg_mask_path'], "numpy")
+                    yolo_results[image_index][face_index]['aligned_cropped_params'] = face_data[ 'aligned_cropped_params']
+                else:
+                    aligned_cropped_image = None
+                    seg_mask = None
+                    yolo_results[image_index][face_index]['aligned_cropped_params'] = None
                 yolo_results[image_index][face_index]['aligned_cropped_image'] = aligned_cropped_image
                 yolo_results[image_index][face_index]['seg_mask'] = seg_mask
-                yolo_results[image_index][face_index]['aligned_cropped_params'] = face_data['aligned_cropped_params']
 
         return batch_frames, orig_imgs, seg_imgs, control_images_inpaint, control_images_p, yolo_results, image_resize_params, image_path
 
