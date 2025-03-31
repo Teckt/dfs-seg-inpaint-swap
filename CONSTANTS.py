c4dddbdad98c6681201c317e357f07476a6d7cc4
Teckt <36281089+Teckt@users.noreply.github.com>
2025-03-30 19:48:50 -0500

    Prefer not using hyper for fill
    fill - 20 steps - 30 seconds
    fill-hyper - 8 steps - 20 seconds

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -14,7 +14,7 @@ USE_SEQUENTIAL_CPU_OFFLOAD = False  # bigger vram savings
 USE_CUSTOM_FLUX = True
 FLUX_CUSTOM_PATH = "Anyfusion/flux-nf4"  # the pretrained path to the custom model
 USE_CUSTOM_FLUX_FILL = True
-FLUX_FILL_CUSTOM_PATH = 'Anyfusion/flux-fill-hyper-nf4'  # the pretrained path to the custom fill model
+FLUX_FILL_CUSTOM_PATH = 'Anyfusion/flux-fill-nf4'  # the pretrained path to the custom fill model
 
 FUSE_HYPER_LORA = False  # whether to fuse hyper lora with transformer
 FUSE_HYPER_ALPHA = 0.125  # set alpha for turbo lora
